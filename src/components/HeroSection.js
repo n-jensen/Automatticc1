bf491037ea13ba22b064b53c90d0676fb2eb3572
hunour <50557542+hunour@users.noreply.github.com>
2021-03-30 00:57:35 -0400

    1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,16 +7,14 @@ import back2 from './video/back2.mp4'
 function HeroSection() {
     return (
         <div className='hero-container'>
-<<<<<<< HEAD
+
             <video autoPlay loop muted>
                 <source src={back2} type="video/mp4" />
             </video>
             {/* <video src="./videos/back2.mp4"  autoPlay={true} loop muted /> */}
-=======
-            <video src="back2.mp4" type="video/mp4" autoPlay={true} loop muted />
->>>>>>> 2c689fffa72f9db5f364f72d6621fca4469dcfe9
+
             <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <p>What are you waiting for hello?</p>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' ButtonSize='btn--large'>GET STARTED</Button>
                 <Button className='btns' buttonStyle='btn--primary' ButtonSize='btn--large'>Watch Trailer <i className='far fa-play-circle' /></Button>
