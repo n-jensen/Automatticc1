0bb851cf075fa593416f16aaf9bcf40240d21e36
hunour <50557542+hunour@users.noreply.github.com>
2021-03-30 01:30:59 -0400

    1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,10 +8,7 @@ function HeroSection() {
     return (
         <div className='hero-container'>
 
-            <video autoPlay loop muted>
-                <source src={back2} />
-            </video>
-            {/* <video src="./videos/back2.mp4"  autoPlay={true} loop muted /> */}
+            <video src="./videos/back2.mp4" type="video/mp4" autoPlay={true} loop muted />
 
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for hello?</p>
