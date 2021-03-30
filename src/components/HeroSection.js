5f139879751598d178686048bbd9b1ae4863e8c8
hunour <50557542+hunour@users.noreply.github.com>
2021-03-30 01:37:21 -0400

    66

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,10 +8,10 @@ function HeroSection() {
     return (
         <div className='hero-container'>
 
-            <video src="./videos/back2.mp4" type="video/mp4" autoPlay={true} loop muted />
+            <video src="./videos/back2.mp4" type="video/mp4" Content-Type="text/html" autoPlay={true} loop muted />
 
             <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for hello?</p>
+            <p>What are you waiting for helo?</p>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' ButtonSize='btn--large'>GET STARTED</Button>
                 <Button className='btns' buttonStyle='btn--primary' ButtonSize='btn--large'>Watch Trailer <i className='far fa-play-circle' /></Button>
