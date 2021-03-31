9e6dc1822ad695d3005aaaa668656eafc6d7314e
hussein247 <hunour@umich.edu>
2021-03-31 14:11:46 -0400

    122
    1wws

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Button } from './Button'
 import '../App.css'
 import './HeroSection.css'
-import back2 from './video/back2.mp4'
+import back2 from 'https://videosauto.s3.us-east-2.amazonaws.com/videos/back2.mp4'
 
 function HeroSection() {
     return (
         <div className='hero-container'>
 
-            <video src="./videos/back2.mp4" type="video/mp4" Content-Type="text/html" autoPlay={true} loop muted />
+            <video src={back2}  autoPlay={true} loop muted />
 
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for helo?</p>
